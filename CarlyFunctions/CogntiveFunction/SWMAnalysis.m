3e88cfbabbd006d4475dc6263f3a6baca6c91a9b
csombric <csombric@u.rochester.edu>
2015-08-19 14:11:08 -0400

    CJS -- Plotting Updated

--- CarlyFunctions/CogntiveFunction/SWMAnalysis.m
+++ CarlyFunctions/CogntiveFunction/SWMAnalysis.m
@@ -1,7 +1,8 @@
-function [SWM SWMlabels] = SWMAnalysis( sub )
+function [SWM, SWMlabels] = SWMAnalysis( sub, plotflag )
 %function [GenAcc GenRT SwitchAcc SwitchRT AccDiff RTDiff]=SWMAnalysis(name)
 
-name=[sub '_SWM.xlsx'];[num,txt,raw]=xlsread(name);
+name=[sub '_SWM.xlsx'];
+[num,txt,raw]=xlsread(name);
 
 %% Measures that I have
 n=size(num, 1);
@@ -22,5 +23,16 @@ correct=nansum([dot1, dot2, dot3], 2);
 
 %General Accuracy
 SWM=mean(correct);
+SWM0=nansum(correct)/sum(~isnan(correct(:)));
+SWM1=nansum(dot1)/sum(~isnan(dot1(:)));
+SWM2=nansum(dot2)/sum(~isnan(dot2(:)));
+SWM3=nansum(dot3)/sum(~isnan(dot3(:)));
+
+if plotflag
+    bar([SWM0 SWM1 SWM2 SWM3])
+    set(gca,'XtickL',['All', '1 Dot', '2 Dots', '3 Dots']);
+    ylabel('%Correct')
+end
+
 SWMlabels={'SPWMAcc'};
 end
